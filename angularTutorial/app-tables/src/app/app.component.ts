8bcff98f74fb66cda4141473dfb83b28ede345d8
deequl <deequl@github.com>
2019-02-06 20:37:15 +0100

    tables

--- angularTutorial/app-tables/src/app/app.component.ts
+++ angularTutorial/app-tables/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-
+  products = [];
+    title = 'Products';
+    ngOnInit() {
+        this.products = this.getProducts();
+    }
+    getProducts() {
+        return [
+            { 'id': '1', 'title': 'Screw Driver', 'price': 400, 'stock': 11 },
+            { 'id': '2', 'title': 'Nut Volt', 'price': 200, 'stock': 5 },
+            { 'id': '3', 'title': 'Resistor', 'price': 78, 'stock': 45 },
+            { 'id': '4', 'title': 'Tractor', 'price': 20000, 'stock': 1 },
+            { 'id': '5', 'title': 'Roller', 'price': 62, 'stock': 15 },
+        ];
+    }
 }
